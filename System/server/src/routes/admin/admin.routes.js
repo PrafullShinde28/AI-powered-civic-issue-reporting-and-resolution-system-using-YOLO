e415bcc33d6b2f10d8744642eacb315828cf2fd2
Varad Kadam <varadkadam3634@gmail.com>
2026-08-16 09:08:27 +0530

    feat(admin): add admin routes

--- System/server/src/routes/admin/admin.routes.js
+++ System/server/src/routes/admin/admin.routes.js
@@ -1,1 +1,71 @@
-// .
+const express =
+    require("express");
+
+const router =
+    express.Router();
+
+const AdminController =
+    require("../../controllers/admin/admin.controller");
+
+const devAuthMiddleware =
+    require("../../middleware/devAuth.middleware");
+
+
+/* ============================================================
+   FINAL COMPLAINT CLOSURE
+============================================================ */
+router.post(
+    "/complaints/:id/override-resolution",
+    AdminController.overrideResolution
+);
+
+router.post(
+
+    "/complaints/:id/close",
+
+    devAuthMiddleware,
+
+    AdminController.closeComplaint
+
+);
+router.post(
+    "/complaints/:id/reassign-worker",
+    devAuthMiddleware,
+    AdminController.reassignWorker
+);
+
+router.post(
+    "/complaints/:id/override-department",
+    devAuthMiddleware,
+    AdminController.overrideDepartment
+);
+
+router.post(
+    "/complaints/:id/override-priority",
+    devAuthMiddleware,
+    AdminController.overridePriority
+);
+
+router.get(
+    "/complaints",
+    devAuthMiddleware,
+    AdminController.getComplaints
+);
+
+
+router.get(
+    "/complaints/:id",
+    devAuthMiddleware,
+    AdminController.getComplaintDetails
+);
+
+
+router.get(
+    "/dashboard/analytics",
+    devAuthMiddleware,
+    AdminController.dashboardAnalytics
+);
+
+
+module.exports =
+    router;
